ac7d6af802d922a3f56640f32d7411fe9a7a088f
Christian <chirstiandave120702@gmail.com>
2025-04-08 20:24:19 +0800

    [ TICKET #1 ] - Create Landing Page for the User

---
+++ app/Application/Pages/MainPage/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./MainPageStyle/MainPage.css"
+import Link from 'next/link'
+
+const MainPage = () => {
+  return (
+    <div>
+        <h1>Main Page</h1>
+        <Link href='/Application/Layouts' >Go back</Link>
+    </div>
+  )
+}
+
+export default MainPage
